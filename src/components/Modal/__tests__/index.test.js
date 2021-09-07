45eb08d4cb923352ea43fabf4136d0e8d45a6bd8
Easton Shaum <Eastonshaum@gmail.com>
2021-09-07 13:30:31 -0600

    added modal tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '..'
+
+
+
+
+
+const mockToggleModal = (image, i) => {
+  setCurrentPhoto({...image, index: i});
+  setIsModalOpen(!isModalOpen);
+}
+
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1
+};
+
+afterEach(cleanup)
+
+describe('Modal component renders', () => {
+  it('renders', () => {
+  render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal}/>);
+});
+
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Modal currentPhoto={currentPhoto} onClose={mockToggleModal}/>)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  describe('Click Event', () => {
+    it('calls onClose handler', () => {
+      const { getByText } = render(<Modal
+        onClose={mockToggleModal}
+        currentPhoto={currentPhoto}
+      />);
+      fireEvent.click(getByText('Close this modal'));
+  
+      expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+  })    
+
+
+
+// it('renders', () => {
+//   const { getByTestId } = render(<Modal currentPhoto={currentPhoto} onClose={toggleModal}/>)
+//   expect(getByTestId('h1tag')).toHaveTextContent('Modal
+//  me')
+// })
+ 
+// it('renders', () => {
+//   const { getByTestId } = render(<Modal currentPhoto={currentPhoto} onClose={toggleModal}/>)
+//   expect(getByTestId('button')).toHaveTextContent('Submit')
+// })
+
+})
